8fdf272583896be47cd80d8fc08a83c1025ddce1
kaycnphngschimswasdi422-oss <kaycnphngschimswasdi422@gmail.com>
2026-07-14 01:13:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 require("dotenv").config();
 
+const fs = require("fs");
+const path = require("path");
+
 const {
-  Client,
-  GatewayIntentBits,
-  Events
+    Client,
+    GatewayIntentBits,
+    Collection
 } = require("discord.js");
 
+require("./database");
+
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildVoiceStates
-  ]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
-client.once(Events.ClientReady, () => {
-  console.log(`✅ Logged in as ${client.user.tag}`);
-});
+client.commands = new Collection();
+
+const eventsPath = path.join(__dirname, "events");
+
+if (fs.existsSync(eventsPath)) {
+    const files = fs.readdirSync(eventsPath);
+
+    for (const file of files) {
+        const event = require(`./events/${file}`);
+        event(client);
+    }
+}
 
 client.login(process.env.TOKEN);
