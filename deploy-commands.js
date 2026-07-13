7d0bb8a3bc5c101ca55fe555f8d7212694c7d2cb
kaycnphngschimswasdi422-oss <kaycnphngschimswasdi422@gmail.com>
2026-07-14 01:24:32 +0700

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,48 @@
+const { REST, Routes } = require("discord.js");
+require("dotenv").config();
+
+const fs = require("fs");
+
+const commands = [];
+
+const commandFiles = fs
+    .readdirSync("./commands")
+    .filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+
+    const command = require(`./commands/${file}`);
+
+    commands.push(command.data.toJSON());
+
+}
+
+const rest = new REST({
+    version: "10"
+}).setToken(process.env.TOKEN);
+
+(async () => {
+
+    try {
+
+        console.log("กำลังลงทะเบียน Slash Commands...");
+
+        await rest.put(
+            Routes.applicationGuildCommands(
+                process.env.CLIENT_ID,
+                process.env.GUILD_ID
+            ),
+            {
+                body: commands
+            }
+        );
+
+        console.log("สำเร็จ");
+
+    } catch (err) {
+
+        console.error(err);
+
+    }
+
+})();
