4f99db26f72d2493f6fc08069a88869078774d24
kaycnphngschimswasdi422-oss <kaycnphngschimswasdi422@gmail.com>
2026-07-14 01:13:18 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,13 +1,36 @@
 const Database = require("better-sqlite3");
 
-const db = new Database("database.db");
+const db = new Database("data/database.sqlite");
 
 db.prepare(`
-CREATE TABLE IF NOT EXISTS users (
-    userId TEXT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    guildId TEXT,
+    userId TEXT UNIQUE,
     username TEXT,
+    points INTEGER DEFAULT 0,
     voiceTime INTEGER DEFAULT 0,
-    points INTEGER DEFAULT 0
+    invites INTEGER DEFAULT 0,
+    lastDaily TEXT DEFAULT "",
+    createdAt TEXT
+)
+`).run();
+
+db.prepare(`
+CREATE TABLE IF NOT EXISTS voice_sessions(
+    userId TEXT PRIMARY KEY,
+    joinTime INTEGER
+)
+`).run();
+
+db.prepare(`
+CREATE TABLE IF NOT EXISTS logs(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    guildId TEXT,
+    userId TEXT,
+    type TEXT,
+    message TEXT,
+    time TEXT
 )
 `).run();
 
